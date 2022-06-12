562f4fc0c014a024ec0d0773b54879ad36224738
GuzikIrinka <106548661+GuzikIrinka@users.noreply.github.com>
2022-06-13 01:32:29 +0300

    Задача2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,17 @@
-﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2; b = 10 -> max = 10
-// a = -9; b = -3 -> max = -3
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 using System;
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
- 
-if(num1 > num2)
+Console.WriteLine ("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
 {
-   Console.WriteLine("Первое число больше второго");
-}
-else if (num1 < num2)
-{
-   Console.WriteLine("Первое число меньше второго");
+    Console.WriteLine("Это чётное число");
 }
 else
 {
-Console.WriteLine("Оба числа равны");
-Console.ReadKey();
+    Console.WriteLine("Это нечётное число");
 }
+
+
